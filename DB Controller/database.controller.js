2e6724564f6f76c6de2ea4353a3aefdd37c62769
phy0hk <phyoheinko.phk1@gmail.com>
2024-12-11 14:53:17 +0630

    Login and Register

--- DB Controller/database.controller.js
+++ DB Controller/database.controller.js
@@ -2,7 +2,7 @@ const {Pool} = require('pg');
 const fs = require('fs').promises;
 const ca_cert = fs.readFile(__dirname+'/ca(1).pem',"utf-8").then((data)=>{return data})
 const pool = new Pool({
-    connectionString: 'postgres://avnadmin:AVNS_YjdmnrZABZIsgRmmWu-@service-postgresql-phkvpn-d7db.e.aivencloud.com:24048/chatappdb',
+    connectionString: process.env.Connection_String,
     ssl:{
         rejectUnauthorized:false
     }
